53e54958d9be405cba7e35b368c0786b6e8a692f
Aaron.Nacua <alnacua@gmail.com>
2017-12-02 17:34:37 -0600

    FilterString updated not completed

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -2,23 +2,43 @@
 import React, {Component} from 'react';
 
 export default class FilterString extends Component{
+    //constructor
     constructor(){
         super();
-
+    //create initial state
         this.state = {
-
-
+            fruit: ['apples', 'bananas','cherries','grapes','pineapple'],
+            userInput: '',
+            filteredArray: []
         }
     }
+        //create handlers
+        //userInput change handler
+        changeHandler(value){
+            this.setState({userInput: value})
+        }
+        filterNames(userInput){
+            var fruit = this.state.fruit;
+            var filteredFruit = [];
+            for(var i = 0; i < fruit.length; i++){
+                if(fruit[i].includes(userInput)){
+                    filteredFruit.push(fruit[i])
+                }
+
+            }
+            this.setState({filteredArray: filteredFruit});
+            }
+        
+        
+    
     render(){
         return(
             <div className="puzzleBox filterStringPB">
-            <span className="puzzleText"></span>
-            <input className="inputLine"></input>
-            <button className="confirmationButton"> </button>
-            <span className="resultsBox filterStringRB"></span>
+            <span className="puzzleText">Fruits: {JSON.stringify(this.state.fruit, null,10)}</span>
+            <input className="inputLine" onChange={ (event) => this.handleChange(event.target.value)}></input>
+            <button className="confirmationButton" onClick={ () => {this.filterNames(this.state.userInput)}}> </button>
+            <span className="resultsBox filterStringRB">Filtered Fruits: {this.state.filteredFruit}</span>
             <h4> Filter String </h4>
-            <p> FilterStrings Component </p>
 
 
 
